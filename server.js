e0c8ddf5711b93f45c828aa366e190dc9ace6fb9
Shaileshkumar Bhadani <shaileshkumar.bhadani@smateso.com>
2025-01-06 13:24:28 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./src/config/db');
+const formRoutes = require('./src/routes/formRoutes');
+const exportRoutes = require('./src/routes/exportRoutes');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/forms', formRoutes);
+app.use('/api/export', exportRoutes);
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
